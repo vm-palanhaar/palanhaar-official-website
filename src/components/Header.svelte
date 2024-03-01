77fb97c0267b0e23fe496c1109eec1261ed05cee
Soumay Gupta <118595898+soumayg9673@users.noreply.github.com>
2024-03-02 01:47:52 +0530

    responsive and adaptable headers

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<header class="flex flex-col relative z-20">
+    <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
+        <a href="/">
+            <h1 class="font-semibold">Palanhaar</h1>
+            <h1 class="font-semibold text-yellow-400">Solutions</h1>
+        </a>
+        <button class="md:hidden grid place-items-center">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+            <a href="/" class="duration-200 hover:text-yellow-400 cursor pointer">Our story</a>
+            <a href="/" class="duration-200 hover:text-yellow-400 cursor pointer">How we help you</a>
+            <a href="/" class="duration-200 hover:text-yellow-400 cursor pointer">Ideas</a>
+            <a href="/" class="duration-200 hover:text-yellow-400 cursor pointer">Insights</a>
+            <a href="/" class="duration-200 hover:text-yellow-400 cursor pointer">Contact us</a>
+        </nav>
+    </div>
+</header>
---
+++ src/components/Hero.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Header from "./Header.svelte";
+import SectionWrapper from "./SectionWrapper.svelte";
+
+</script>
+<SectionWrapper>
+    <Header/>
+</SectionWrapper>
